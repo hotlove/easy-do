477c13be8abd1da6ba5df42bdbff85d8682c1359
hotlove <guolejian@126.com>
2019-12-05 20:13:00 +0800

    first commit

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
+Vue.use(MuseUI)
 
 Vue.config.productionTip = false
 
