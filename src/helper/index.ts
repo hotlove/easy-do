883260d29d2246d22ac0356e57c81af6078a659f
guolejian <guolejian@126.com>
2019-12-20 14:02:15 +0800

    引入lodash

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import request from "@/http/index";
 import moment from "moment";
+import MuseUI from 'muse-ui';
+// import _ from "lodash";
+import 'muse-ui/dist/muse-ui.css'
+
+Vue.use(MuseUI);
 
 Vue.use(v => {
     v.prototype.$http = request;
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import MuseUI from 'muse-ui'
-import 'muse-ui/dist/muse-ui.css'
 import '@/helper/index'
-Vue.use(MuseUI);
 
 Vue.config.productionTip = false
 
