eba38c1524f8c549b998f2f8fd318cf9b63d972e
guolejian <guolejian@126.com>
2019-12-25 18:21:16 +0800

    增加无边框效果

--- src/components/CloseNavigation.vue
+++ src/components/CloseNavigation.vue
@@ -1,23 +1,31 @@
 <template>
-    <div class="dragger-element">
+    <div class="dragger-element" :class="heigthValue">
 <!--        <div ></div>-->
     </div>
 </template>
 <script lang="ts">
-    import {Component, Vue} from "vue-property-decorator";
+    import {Component, Prop, Vue} from "vue-property-decorator";
 
     @Component
     export default class CloseNavigation extends Vue {
+        @Prop({ default: 20 })
+        height !: number
+
+        get heigthValue() {
+            console.log(this.height)
+            return {
+                'height': this.height + 'px',
+                'line-height': this.height + 'px'
+            }
+        }
     }
 </script>
 <style lang="scss">
     .dragger-element {
         -webkit-app-region: drag;
-        height: 25px;
-        line-height: 25px;
-        border: 1px solid red;
         width: 100%;
         position: fixed;
         top: 0;
+        border: 1px solid red;
     }
 </style>
