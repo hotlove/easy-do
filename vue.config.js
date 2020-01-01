acbfe048c450e417e3b0a4576060925ac9b1c6ee
hotlove_linx <guolejian@126.com>
2020-01-01 22:23:20 +0800

    electron打包

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    pluginOptions: {
+        electronBuilder: {
+            builderOptions: {
+                "appId": "com.guo.app",
+                "productName":"e-do",//项目名，也是生成的安装文件名，即aDemo.exe
+                "copyright":"Copyright © 2019",//版权信息
+                "directories":{
+                    "output":"./dist"//输出文件路径
+                },
+                "win":{//win相关配置
+                    "icon":"./static/icon/e-do256.ico",//图标，当前图标在根目录下，注意这里有两个坑
+                    "target": [
+                        {
+                            "target": "nsis",//利用nsis制作安装程序
+                            "arch": [
+                                "x64",//64位
+                                "ia32"//32位
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+
+    }
+}
