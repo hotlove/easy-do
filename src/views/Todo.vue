0e7cad528fe8b39ce3e4bfe040e29cc7f62d70a9
hotlove_linx <guolejian@126.com>
2020-01-01 18:34:41 +0800

    1.修改日历尺寸 2.引入animate.css

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,12 +1,14 @@
 <template>
     <div style="float: left">
         <div class="todo-home">
+            <!-- todo导航 -->
             <div class="todo-home-nav">
                 <close-navigation :height="25"></close-navigation>
                 <div class="todo-home-nav-content">
                     <el-input v-model="todoItemSearch" placeholder="搜索" size="mini" prefix-icon="el-icon-search"
                               @focus="showRecentSearch">
-                        <i v-if="!showDatePicker" slot="suffix" class="el-icon-circle-close el-input__icon" style="cursor: pointer" @click="clearSearch">
+                        <i v-if="!showDatePicker" slot="suffix" class="el-icon-circle-close el-input__icon"
+                           style="cursor: pointer" @click="clearSearch">
                         </i>
                     </el-input>
 
@@ -20,15 +22,35 @@
                 </div>
 
             </div>
+
+            <!-- todo内容列表 -->
             <div class="todo-home-body">
                 <close-navigation :height="25" :show-close="true"></close-navigation>
                 <div class="todo-home-body-content">
-                    <div class="todo-body-content-input">
-
+                    <!--内容标题 用于条件控制-->
+                    <div class="todo-body-title">
+                        <el-radio-group v-model="completedControl" size="mini">
+                            <el-radio-button :label="false">未完成</el-radio-button>
+                            <el-radio-button :label="true">已完成</el-radio-button>
+                        </el-radio-group>
                     </div>
-                    <div class="todo-body-content-list"></div>
-                </div>
+                    <div class="todo-body-content-list">
+                        <div class="todo-uncompleted todo-container" v-if="!completedControl">
+                            <div class="todo-uncompleted-input">
+                                <el-input type="textarea"
+                                          ref="todoInput"
+                                          @input.native="heightMonitor"
+                                          autosize
+                                          placeholder="请输入内容"
+                                          v-model="todoItem"/>
+                            </div>
+                            <div class="todo-uncompleted-list" :style="uncompletedStyle">
 
+                            </div>
+                        </div>
+                        <div class="todo-completed todo-container" v-if="completedControl"></div>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -36,12 +58,12 @@
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from "vue-property-decorator";
+    import {Component, Vue, Watch} from "vue-property-decorator";
     import {Getter, Action} from "vuex-class";
     import CloseNavigation from "@/components/CloseNavigation.vue";
 
     @Component({
-        components:{
+        components: {
             CloseNavigation
         }
     })
@@ -55,17 +77,35 @@
         todoItemSearch: string = "";
         date: any = new Date();
         showDatePicker: boolean = true; // 展示日历
+        completedControl: boolean = false; // 控制是否完成
+        todoItem: string = ""; // 填写得内容
+        uncompletedStyle: any = { // 未完成todo列表样式 用于自动控制高度
+            height: 'calc(100vh - 115px)'
+        };
+
+        @Watch('uncompletedStyle')
+        uncompletedMethod() {
+            return {
+                height: 'calc(100vh - 115px)'
+            }
+        }
 
         // 展示最近修改东西
-        private showRecentSearch(): void {
+        public showRecentSearch(): void {
             // 1.停止展示日历切换最近修改面板
             this.showDatePicker = false;
         }
 
-        private clearSearch(): void {
+        public clearSearch(): void {
             this.showDatePicker = true;
         }
 
+        // 监控输入框高度变化
+        public heightMonitor(event: any): void {
+            let heigthDiff: number = event.target.clientHeight + 65 + 19;
+            // 获取图片
+            this.uncompletedStyle.height = 'calc(100vh - ' + heigthDiff + 'px)';
+        }
     }
 </script>
 <style lang="scss">
@@ -110,19 +150,45 @@
             height: 100%;
             float: right;
             background: #F5F5F5;
-            padding: 60px 0 0 0;
+            padding: 25px 0 0 0;
 
             .todo-home-body-content {
-                border: 1px solid red;
-                height: calc(100vh - 60px);
-                padding: 10px 20px;
+                height: calc(100vh - 20px);
+                padding: 0px 10px;
 
-                .todo-body-content-input {
-
+                .todo-body-title {
+                    height: 23px;
                 }
 
                 .todo-body-content-list {
+                    margin-top: 15px;
+                    .todo-container {
+                        height: calc(100vh - 65px);
+                    }
 
+                    .todo-uncompleted {
+
+
+                        .todo-uncompleted-input {
+                            font-size: 12px;
+
+                            .el-textarea__inner {
+                                border: none !important;
+                                border-bottom: 1px solid #DCDFE6 !important;
+                                border-radius: 0;
+                                background-color: transparent;
+                            }
+                        }
+
+                        .todo-uncompleted-list {
+                            border: 1px solid red;
+                            margin-top: 15px;
+                        }
+                    }
+
+                    .todo-completed {
+
+                    }
                 }
             }
         }
