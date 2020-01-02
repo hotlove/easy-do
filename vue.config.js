b1a09fbccfe54cdd1eae7fa6f696c733e589e453
guolejian <guolejian@126.com>
2020-01-02 15:21:44 +0800

    修改资源路径

--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,16 @@ module.exports = {
         electronBuilder: {
             builderOptions: {
                 "appId": "com.guo.app",
-                "productName":"e-do",//项目名，也是生成的安装文件名，即aDemo.exe
-                "copyright":"Copyright © 2019",//版权信息
-                "directories":{
-                    "output":"./dist"//输出文件路径
+                "productName": "e-do",//项目名，也是生成的安装文件名，即aDemo.exe
+                "copyright": "Copyright © 2019",//版权信息
+                "directories": {
+                    "output": "./dist"//输出文件路径
                 },
-                "win":{//win相关配置
-                    "icon":"./src/assets/icon/e-do256.ico",//图标，当前图标在根目录下，注意这里有两个坑
+                "files": [
+                    "./**/*"
+                ],
+                "win": {//win相关配置
+                    "icon": "./src/assets/icon/e-do256.ico",//图标，当前图标在根目录下，注意这里有两个坑
                     "target": [
                         {
                             "target": "nsis",//利用nsis制作安装程序
@@ -22,6 +25,5 @@ module.exports = {
                 }
             }
         }
-
     }
-}
+};
